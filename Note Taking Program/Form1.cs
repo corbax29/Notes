954be6a132a8c7c0de0a9c650f4ee0ccb1e57f2a
corbax29 <73996815+corbax29@users.noreply.github.com>
2022-02-25 10:47:16 +0100

    Add files via upload

---
+++ Note Taking Program/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Note_Taking_Program
+{
+    public partial class Form1 : Form
+    {
+        DataTable table;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            table = new DataTable();
+            table.Columns.Add("Title", typeof(string));
+            table.Columns.Add("Messages", typeof(string));
+
+            dataGridView1.DataSource = table;
+
+            dataGridView1.Columns["Messages"].Visible = false;
+            dataGridView1.Columns["Title"].Width = 285;
+        }
+
+        private void bttNew_Click(object sender, EventArgs e)
+        {
+            txtTitle.Clear();
+            txtMsg.Clear();
+        }
+
+        private void bttSave_Click(object sender, EventArgs e)
+        {
+            table.Rows.Add(txtTitle.Text, txtMsg.Text);
+            txtTitle.Clear();
+            txtMsg.Clear();
+        }
+
+        private void bttRead_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+
+            if(index > -1)
+            {
+                txtTitle.Text = table.Rows[index].ItemArray[0].ToString();
+                txtMsg.Text = table.Rows[index].ItemArray[1].ToString();
+            }
+        }
+
+        private void bttDel_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+
+            table.Rows[index].Delete();
+        }
+
+       
+    }
+}
